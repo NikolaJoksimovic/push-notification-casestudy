a9dcc0e65b133c38db96408eff8720fce6436eff
unknown <nikolajoksimovic95@gmail.com>
2023-01-14 14:29:20 +0100

    cange send to showNotification

--- server/server.js
+++ server/server.js
@@ -11,9 +11,18 @@ const vapidPublicKey = process.env.VAPID_PUBLIC_KEY;
 
 webpush.setVapidDetails(
   "mailto:examle@test.org",
-  vapidPublicKey,
-  vapidPrivateKey
+  "BFEklZmIr03GoqLLEp6qw2iC7Ful36SFLC8nR6vgY1ju9E7BFgTANwLnz1UHiZPVxsuyfNqJC7XXlC5UvRALTjE",
+  "gwxNC9Fu7LtTW2xg7pt0r0ZNCAjCSqjQTNJQPzSqrBc"
 );
 
-let sub = {};
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/d8zS6l_6kmU:APA91bHiZDaq4IGMYenVrPzq_N1wKr0_sN9gf15pmg80KPMtv_DvOQbfjNBGOif01tgElmHspO2WLJ4vaSEBqQowNP1tiNn19OdhqGqbTTq4r8KJnrqUlLvp4tq2b8EbJtt4VFN8OGVz",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BGsG5IYVxl8wJZeXv-P26BiZ83QVXDpO84lGpWehLY4qaDimi1dxMTEIDbUos0q4AwrgQ0RaQzTyYFVsO1KDIn4",
+    auth: "-A2B0Q6V0tcm1uTrEtPEJQ",
+  },
+};
 webpush.sendNotification(sub, "test message");
